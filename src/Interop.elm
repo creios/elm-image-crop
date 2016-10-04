144771cb49d739589d866649d68f4228e3621a9b
Jonas Schürmann <schuermann@creios.net>
2016-10-04 10:28:35 +0200

    Add Interop module

---
+++ src/Interop.elm
@@ -0,0 +1,38 @@
+port module Interop exposing (main)
+
+import Html.App
+import ImageCropper
+
+
+type alias Flags =
+    { imageSize: ImageCropper.Size
+    , selection: ImageCropper.Rectangle
+    }
+
+
+init : Flags -> ( ImageCropper.Model, Cmd ImageCropper.Msg )
+init {imageSize,selection} =
+    ( ImageCropper.init imageSize selection
+    , Cmd.none
+    )
+
+
+main : Program Flags
+main =
+    Html.App.programWithFlags
+        { init = init
+        , view = ImageCropper.view
+        , update = update
+        , subscriptions = ImageCropper.subscriptions
+        }
+
+
+update : ImageCropper.Msg -> ImageCropper.Model -> ( ImageCropper.Model, Cmd ImageCropper.Msg )
+update msg model =
+    let
+        (newModel, _) = ImageCropper.update msg model
+    in
+        (newModel, selectionChanged newModel.selection)
+
+
+port selectionChanged : ImageCropper.Rectangle -> Cmd msg

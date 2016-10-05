6db4824c8e9d93e7804e59982393ec4dae0f76d1
Jonas Schürmann <schuermann@creios.net>
2016-10-05 16:08:52 +0200

    Enable live responsiveness via the Interop module and ports

--- src/Interop.elm
+++ src/Interop.elm
@@ -1,5 +1,6 @@
-port module Interop exposing (main)
+port module Interop exposing (main, viewportChanged)
 
+import Html exposing (Html)
 import Html.App
 import ImageCropper
 
@@ -11,30 +12,57 @@ type alias Flags =
     }
 
 
-init : Flags -> ( ImageCropper.Model, Cmd ImageCropper.Msg )
+init : Flags -> ( ImageCropper.Model, Cmd Msg )
 init { image, cropAreaWidth, selection } =
     ( ImageCropper.init image cropAreaWidth selection
     , Cmd.none
     )
 
 
+type Msg
+    = ViewportChanged Int
+    | ImageCropperMsg ImageCropper.Msg
+
+
 main : Program Flags
 main =
     Html.App.programWithFlags
         { init = init
-        , view = ImageCropper.view
+        , view = view
         , update = update
-        , subscriptions = ImageCropper.subscriptions
+        , subscriptions = subscriptions
         }
 
 
-update : ImageCropper.Msg -> ImageCropper.Model -> ( ImageCropper.Model, Cmd ImageCropper.Msg )
+update : Msg -> ImageCropper.Model -> ( ImageCropper.Model, Cmd Msg )
 update msg model =
     let
-        ( newModel, _ ) =
-            ImageCropper.update msg model
+        newModel =
+            case msg of
+                ViewportChanged width ->
+                    { model | cropAreaWidth = width }
+
+                ImageCropperMsg msg ->
+                    fst (ImageCropper.update msg model)
     in
         ( newModel, selectionChanged newModel.selection )
 
 
+view : ImageCropper.Model -> Html Msg
+view model =
+    Html.App.map ImageCropperMsg (ImageCropper.view model)
+
+
+subscriptions : ImageCropper.Model -> Sub Msg
+subscriptions model =
+    let
+        imageCropperSubs =
+            Sub.map ImageCropperMsg <| ImageCropper.subscriptions model
+    in
+        Sub.batch [ viewportChanged ViewportChanged, imageCropperSubs ]
+
+
 port selectionChanged : Maybe ImageCropper.Rectangle -> Cmd msg
+
+
+port viewportChanged : (Int -> msg) -> Sub msg
